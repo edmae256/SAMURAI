6a3307523a3e293f0ea10a9388dd8b129ed20bda
user1 <aor01gmgm330@gmail.com>
2011-05-24 00:28:25 +0900

    キャラクタイメージ読み込みまで？

--- NinjaOfTheMoment/src/jp/yasay/data/Character.java
+++ NinjaOfTheMoment/src/jp/yasay/data/Character.java
@@ -8,6 +8,10 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.widget.ImageView;
+
 
 
 /**
@@ -23,7 +27,11 @@ public class Character implements Closeable {
 	private Map<CharacterState,ImageData[]> drawMaps = new HashMap<CharacterState,ImageData[]>(8);
 
 	private CharacterState state;
+	private String name;
 	private int count;
+
+
+	private ImageView image;
 
 	/**
 	 * 状態のリセット
@@ -33,6 +41,14 @@ public class Character implements Closeable {
 		this.count = 0;
 	}
 
+	public void Load(Context context){
+		if(this.name != null && 0 < this.name.length()){
+			image = new ImageView(context);
+			//TODO: 名前から取得したい場合、どこに配置したものどう参照すればよいのだろうか…
+			image.setImageBitmap(BitmapFactory.decodeFile(this.name));
+		}
+	}
+
 	/**
 	 * 初期化
 	 */
@@ -40,23 +56,47 @@ public class Character implements Closeable {
 		reset();
 	}
 
-
-
-
-	/**
-	 * ステータス取得
-	 * @return
-	 */
+	/*** java properties  start ***/
+	public ImageData[] getImageData(CharacterState state){
+		if(drawMaps.containsKey(state)) return drawMaps.get(state);
+		return EmptyImageDatas;
+	}
+	public void setName(String name){
+		this.name = name;
+	}
+	public String getName(){
+		return this.name;
+	}
+	public void setWait(ImageData[] value){
+		drawMaps.put(CharacterState.Wait,value);
+	}
+	public ImageData[] getWait(){
+		return getImageData(CharacterState.Wait);
+	}
+	public void setWin(ImageData[] value){
+		drawMaps.put(CharacterState.Win, value);
+	}
+	public ImageData[] getWin(){
+		return getImageData(CharacterState.Win);
+	}
+	public void setLoss(ImageData[] value){
+		drawMaps.put(CharacterState.Loss,value);
+	}
+	public ImageData[] getLoss(){
+		return getImageData(CharacterState.Loss);
+	}
+	public void setAttack(ImageData[] value){
+		drawMaps.put(CharacterState.Attack, value);
+	}
+	public ImageData[] getAttack(){
+		return getImageData(CharacterState.Attack);
+	}
 	public CharacterState getState(){ return this.state; }
-	/**
-	 * ステータス変更
-	 * @param st
-	 */
 	public void setState(CharacterState st){
 		this.state = st;
 		if(!drawMaps.containsKey(st)) drawMaps.put(st,EmptyImageDatas);
 	}
-
+	/*** java properties  end  ***/
 
 	/* (非 Javadoc)
 	 * @see java.io.Closeable#close()
