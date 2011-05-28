213de685ddee6b89bd009480a97b3d99f0b42b0f
hare <hare_1110@yahoo.co.jp>
2011-05-28 01:30:22 -0700

    プロジェクトを削除

--- NinjaOfTheMoment/src/jp/yasay/Logo.java
+++ NinjaOfTheMoment/src/jp/yasay/Logo.java
@@ -1,13 +1,28 @@
 package jp.yasay;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Logo extends Activity {
-    /** Called when the activity is first created. */
+
+	// TODO 切替時にアニメーション。フェードイン、アウト？
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.logo);
+
+        Handler handler = new Handler();
+
+        // TODO 次の画面に遷移するまでの時間を設定。
+        handler.postDelayed(new Runnable() {
+			
+			@Override
+			public void run() {
+				startActivity(new Intent(Logo.this, MainMenu.class));
+				finish();
+			}
+		}, 1000);
     }
 }
--- NinjaOfTheMoment/src/jp/yasay/MainMenu.java
+++ NinjaOfTheMoment/src/jp/yasay/MainMenu.java
@@ -2,12 +2,52 @@ package jp.yasay;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainMenu extends Activity {
-    /** Called when the activity is first created. */
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.main_menu);
+        setListeners();
+    }
+    
+    protected void onStart(){
+    	super.onStart();
+    	// TODO BGM !!!!!!!!!!!!!!!
+    }
+    
+    private void setListeners(){
+    	
+    	Button single = (Button) findViewById(R.id.single);
+    	Button muitl  = (Button) findViewById(R.id.multi);
+    	Button scores = (Button) findViewById(R.id.scores);
+    	
+    	single.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+			}
+		});
+    	
+    	muitl.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+			}
+		});
+    	
+    	scores.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+			}
+		});
     }
 }
